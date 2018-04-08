57d04d7d7f2196a2065dcc2272eba91c56ff0368
Dominic van Berkel <dominic@baudvine.net>
2018-04-08 08:40:36 +0200

    cargo fmt

--- src/math.rs
+++ src/math.rs
@@ -1,7 +1,7 @@
 // Generic math helpers, mostly for the model. Much of this is re-exporting parts of the piston
 // graphics::math module.
 
-pub use graphics::math::{Matrix2d,Vec2d,orient,sub,add};
+pub use graphics::math::{add, orient, sub, Matrix2d, Vec2d};
 use std::ops::Mul;
 use uom::si::f64::*;
 use vecmath::vec2_normalized;
@@ -16,9 +16,9 @@ pub fn normalized(v: Vec2d<Length>) -> Vec2d<f64> {
 }
 
 pub fn mul_scalar<T, U, V>(t: Vec2d<T>, u: U) -> Vec2d<V>
-    where
-        T: Copy + Mul<U, Output = V>,
-        U: Copy,
+where
+    T: Copy + Mul<U, Output = V>,
+    U: Copy,
 {
     [t[0] * u, t[1] * u]
 }
