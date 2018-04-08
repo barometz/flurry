a1aebec4daa700fa2276c0a23ed63bb7fbbed3c3
Dominic van Berkel <dominic@baudvine.net>
2018-04-08 09:02:14 +0200

    some tests

--- src/math.rs
+++ src/math.rs
@@ -15,10 +15,37 @@ pub fn normalized(v: Vec2d<Length>) -> Vec2d<f64> {
     }
 }
 
+/// Generic scalar multiplication.
+///
+/// #Examples
+/// ```
+/// assert_eq!([10, 15], mul_scalar([2, 3], 5));
+/// ```
 pub fn mul_scalar<T, U, V>(t: Vec2d<T>, u: U) -> Vec2d<V>
 where
     T: Copy + Mul<U, Output = V>,
     U: Copy,
 {
     [t[0] * u, t[1] * u]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mul_scalar_0() {
+        assert_eq!([0, 0], mul_scalar([0, 0], 0));
+        assert_eq!([0, 0], mul_scalar([1, 4], 0));
+    }
+
+    #[test]
+    fn mul_scalar_1() {
+        assert_eq!([1, 4], mul_scalar([1, 4], 1));
+    }
+
+    #[test]
+    fn mul_scalar_values() {
+        assert_eq!([10, 15], mul_scalar([2, 3], 5));
+    }
 }
